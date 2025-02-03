22f6970084360ee3e66e33185abdde864aa90e1c
Frerich Raabe <frerich.raabe@gmail.com>
2025-02-03 22:43:11 +0100

    Add AutoUpdater.Storage behaviour
    This is a first shot at a behaviour describing how a 'storage' of
    releases works. I plan to just have an S3 implementation for now, but
    this should also be useful for tests.

---
+++ lib/auto_updater/storage.ex
@@ -0,0 +1,12 @@
+defmodule AutoUpdater.Storage do
+  @callback desired_version() :: {:ok, String.t()} | {:error, any()}
+  @callback download_release(version :: String.t()) :: {:ok, Path.t()} | {:error, any()}
+
+  def desired_version(), do: impl().desired_version()
+
+  def download_release(version) when is_binary(version) do
+    impl().download_release(version)
+  end
+
+  def impl, do: Application.fetch_env!(:auto_updater, :storage)
+end
