93bcdb3b3453a469609024681574d529579167cd
Frerich Raabe <frerich.raabe@gmail.com>
2025-02-03 22:15:45 +0100

    mix new auto_updater

---
+++ lib/auto_updater.ex
@@ -0,0 +1,18 @@
+defmodule AutoUpdater do
+  @moduledoc """
+  Documentation for `AutoUpdater`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AutoUpdater.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/auto_updater_test.exs
@@ -0,0 +1,8 @@
+defmodule AutoUpdaterTest do
+  use ExUnit.Case
+  doctest AutoUpdater
+
+  test "greets the world" do
+    assert AutoUpdater.hello() == :world
+  end
+end
