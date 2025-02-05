35702501660e43d60e4c6c235dd97d391cb72da6
Frerich Raabe <frerich.raabe@gmail.com>
2025-02-05 22:15:08 +0100

    Start AutoUpdater.Application on startup

--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule AutoUpdater.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {AutoUpdater.Application, {}}
     ]
   end
 
