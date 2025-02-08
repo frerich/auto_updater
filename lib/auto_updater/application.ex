a779fd4741ab9d626675c241fe40054c95ae7a39
Frerich Raabe <frerich.raabe@gmail.com>
2025-02-08 22:00:06 +0100

    Optionally launch SecretsWatcher

--- lib/auto_updater/application.ex
+++ lib/auto_updater/application.ex
@@ -1,12 +1,22 @@
 defmodule AutoUpdater.Application do
   use Application
 
+  require Logger
+
   def start(_, _) do
     opts = [strategy: :one_for_one, name: AutoUpdater.Supervisor]
 
     children = [
       AutoUpdater.ReleaseWatcher
     ]
+
+    children =
+      if Application.get_env(:auto_updater, :secrets) do
+        children ++ [AutoUpdater.SecretsWatcher]
+      else
+        Logger.info("secrets not configured for auto_updater, disabling polling for new secrets")
+        children
+      end
 
     Supervisor.start_link(children, opts)
   end
